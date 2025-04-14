f5325748b8819a31c87d32d25e52d89bc7038c77
SarahSoutoul <soutoulsarah.uk@gmail.com>
2025-04-13 18:06:07 -0600

    fix responsiveness

--- app/components/sections/hero-section/index.tsx
+++ app/components/sections/hero-section/index.tsx
@@ -11,19 +11,19 @@ export default function HeroSection() {
         <h1 className="text-xl font-bold my-5">I am a <span className="typed-[educator|coder|writer] typed-caret"></span></h1>
         <h2 className='my-5'>Empowering women to break into and thrive in tech, I mentor aspiring developers, help them build confidence, navigate their unique journeys, and succeed in the ever-evolving tech landscape</h2>
       </hgroup>
-      <div className="buttons my-5">
+      <div className="buttons my-5 text-xs sm:text-sm flex">
         <Link href="https://calendly.com/soutoulsarah-uk/1-1-mentor-session">
-          <button className="rounded-md bg-[#424242] py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+          <button className="rounded-md bg-[#424242] p-2 md:px-4 border border-transparent text-center text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
               Book an intro session
           </button>
         </Link>
         <Link href="#what-i-offer">
-          <button className="rounded-md bg-white py-2 px-4 border border-transparent text-center text-sm text-slate-800 transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 hover:text-white active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
+          <button className="rounded-md bg-white p-2 md:px-4 border border-transparent text-center text-slate-800 transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 hover:text-white active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">
             Learn more
           </button>
         </Link>
       </div>
-      <div className="flex gap-1">
+      <div className="sm:flex text-xs sm:text-sm gap-1">
           <p>Already working with me?</p><Link href="/sign-in" className="font-semibold hover:underline">Log in to your dashboard</Link>
         </div>
 	</section>
